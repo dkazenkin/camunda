791c9ab7dbb5a9de922a313088654edac847c58e
dkazenkin <70374230+dkazenkin@users.noreply.github.com>
2021-02-12 16:17:04 +0500

    Update camunda-modeler.sh

--- camunda-modeler.sh
+++ camunda-modeler.sh
@@ -1,3 +1,15 @@
  #!/bin/bash
+ #
+ # > chmod +x ./camunda-modeler
+ #
+ # если при запуске ошибка 
+ # [14158:0212/150132.084042:FATAL:setuid_sandbox_host.cc(157)] 
+ # The SUID sandbox helper binary was found, but is not configured correctly. 
+ # Rather than run without sandboxing I'm aborting now. You need to make sure 
+ # that /home/user/camunda-modeler/chrome-sandbox is owned by root and has mode 4755.
+ #
+ # > sudo chown root ./chrome-sandbox
+ # > sudo chmod 4755 ./chrome-sandbox
+ #
 
 ./camunda-modeler >/dev/null 2>&1 &
